cc9d835ca84aaabbabd67081c3b86611ac07f66e
SamirIngley <samir.ingle7@gmail.com>
2020-11-24 19:06:57 -0800

    goreleaser

---
+++ generated.go
@@ -0,0 +1,8 @@
+package main
+var dirFile = map[string][]byte{
+"dirFile.txt": []byte(``),
+};var profFile = map[string][]byte{
+"profFile.txt": []byte(``),
+};var appsFile = map[string][]byte{
+"appsFile.txt": []byte(``),
+};
